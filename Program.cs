9b8129fd6311637afff9e5b82dd747a46a85de4a
Travis Guthrie <Gu3@nowhwere>
2022-01-26 01:05:31 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BoolFunctionProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static bool IsNegPos(int num1, int num2) => (num1 < 0 && num2 > -1) ? true : false;
+
+            int num1 = -1;
+            int num2 = 3;
+            Console.Write($"\"{num1}\" is negative & \"{num2}\" is positive: {IsNegPos(num1, num2)}\n");
+
+            num1 = 2;
+            num2 = 0;
+            Console.Write($"\"{num1}\" is negative & \"{num2}\" is positive: {IsNegPos(num1, num2)}\n");
+
+            num1 = 0;
+            num2 = 1;
+            Console.Write($"\"{num1}\" is negative & \"{num2}\" is positive: {IsNegPos(num1, num2)}");
+            
+            
+        }
+    }
+}
